4438d5461789a6bb4ae5650d3fc48f7d98d74954
Unknown User <artyomshul@gmail.com>
2024-11-10 18:10:05 +0300

    Add services

---
+++ Core/InactivePassports.Services/DatabaseUpdate/DataDownloadService.cs
@@ -0,0 +1,195 @@
+﻿using System.Text;
+using System.Globalization;
+using InactivePassports.Contracts.DatabaseUpdate;
+using InactivePassports.Data.Configurations;
+using InactivePassports.Data.Entities.File;
+using Google.Apis.Drive.v3;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util.Store;
+using Google.Apis.Services;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace InactivePassports.Services.DatabaseUpdate
+{
+    public class DataDownloadService : IDataDownload
+    {
+        private readonly DataDownloadOptions _options;
+
+        public DataDownloadService(DataDownloadOptions options)
+        {
+            _options = options;
+        }
+
+        public void DownloadFile()
+        {            
+            string[] scopes = { DriveService.Scope.DriveReadonly };
+            string ApplicationName = _options.ApplicationName;
+
+            UserCredential credential;
+
+            using (FileStream stream = new FileStream(_options.CredentialsPath, FileMode.Open, FileAccess.Read))
+            {
+                string credPath = _options.TokensPath;
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            DriveService service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            var request = service.Files.Get(_options.FileId);
+
+            using (var memoryStream = new MemoryStream())
+            {
+                request.MediaDownloader.ProgressChanged += progress =>
+                {
+                    if (progress.Status == Google.Apis.Download.DownloadStatus.Failed)
+                    {
+                        throw new FileNotFoundException("Не удалось загрузить файл.");
+                    }
+                };
+
+                request.Download(memoryStream);
+
+                using (FileStream fileStream = new FileStream(_options.DataPath, FileMode.Create, FileAccess.Write))
+                {
+                    memoryStream.WriteTo(fileStream);
+                }
+            }
+        }
+
+        public HashSet<Passport> ReadPassports(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("Файл не найден.", path);
+            }
+
+            int lineCount = LineCountCheck(path);
+
+            HashSet<Passport> passports = new HashSet<Passport>(lineCount - 1);
+
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                using (CsvReader csv = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                {
+                    foreach (Passport passport in csv.GetRecords<Passport>())
+                    {
+                        passports.Add(passport);
+                    }
+                }
+            }
+
+            return passports;
+        }
+
+        public IEnumerable<List<Passport>> ReadFile()
+        {
+            string path = _options.DataPath;
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("Файл Data.csv не найден.", path);
+            }
+
+            var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+                Delimiter = ",",
+                Encoding = Encoding.UTF8
+            };
+
+            int lineCount = LineCountCheck(path);
+            int returnCount = 0;
+            int sizeCollection = _options.SizeCollection;
+
+            List<Passport> passports = new List<Passport>(sizeCollection);
+
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                streamReader.ReadLine();
+
+                using (CsvReader csv = new CsvReader(streamReader, csvConfig))
+                {
+                    while (csv.Read())
+                    {
+                        if (csv.TryGetField<int>(0, out var series) && csv.TryGetField<int>(1, out var number))
+                        {
+                            if (ValidationCheck(series, number))
+                            {
+                                passports.Add(new Passport
+                                {
+                                    Series = series,
+                                    Number = number
+                                });
+
+                                if (passports.Count == sizeCollection)
+                                {
+                                    yield return new List<Passport>(passports);
+
+                                    returnCount++;
+
+                                    passports.Clear();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (passports.Count > 0)
+            {
+                yield return passports;
+
+                returnCount++;
+            }
+
+            if (returnCount == 0)
+            {
+                throw new InvalidDataException("Файл Data.csv не содержит необходимые данные.");
+            }
+        }
+
+        private static bool ValidationCheck(int series, int number)
+        {
+            if (series == 0 || number == 0)
+            {
+                return false;
+            }
+
+            int seriesDigits = (int)Math.Log10(Math.Abs(series)) + 1;
+            int numberDigits = (int)Math.Log10(Math.Abs(number)) + 1;
+
+            if (seriesDigits <= 4 && numberDigits <= 6)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static int LineCountCheck(string path)
+        {
+            int count = 0;
+
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                while (streamReader.ReadLine() != null)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Core/InactivePassports.Services/DatabaseUpdate/DataUpdateService.cs
@@ -0,0 +1,258 @@
+﻿using System.Globalization;
+using InactivePassports.Contracts.DatabaseUpdate;
+using InactivePassports.Contracts.Repositories;
+using InactivePassports.Data.Configurations;
+using InactivePassports.Data.Entities.File;
+using CsvHelper;
+
+namespace InactivePassports.Services.DatabaseUpdate
+{
+    public class DataUpdateService : IDataUpdate
+    {
+        private readonly DataUpdateOptions _options;
+        private readonly IDatabaseUpdate _repository;
+
+        private Queue<Passport> _passportsForInsert;
+        private Queue<Passport> _passportsForActivation;
+        private Queue<Passport> _passportsForDeactivation;
+
+        private HashSet<Passport> _passportsInactive;
+        private HashSet<Passport> _passportsActive;
+        private HashSet<Passport> _passportsInactiveActual;
+
+        private readonly SemaphoreSlim _semaphore;
+        private readonly int _batchSize;
+
+        public DataUpdateService(IDatabaseUpdate databaseUpdateRepository, DataUpdateOptions options)
+        {
+            _passportsForInsert = new Queue<Passport>();
+            _passportsForActivation = new Queue<Passport>();
+            _passportsForDeactivation = new Queue<Passport>();
+            _passportsInactiveActual = new HashSet<Passport>();
+
+            _options = options;
+            _repository = databaseUpdateRepository;
+
+            _semaphore = new SemaphoreSlim(_options.SemaphoreSize);
+            _batchSize = _options.BatchSize;
+        }
+
+        public void GetData(HashSet<Passport> passportsInactive, HashSet<Passport> passportsActive)
+        {
+            _passportsInactive = passportsInactive;
+            _passportsActive = passportsActive;
+        }
+
+        public void UpdateData(IEnumerable<List<Passport>> passports)
+        {
+            int count = 0;
+
+            foreach (var passport in passports)
+            {
+                CompareData(passport);
+
+                count++;
+
+                Console.WriteLine(count);
+                Console.WriteLine(passport.Count);
+            }
+
+            CompareRemainData();
+            CopyData();
+
+            Console.WriteLine("Готово");
+        }
+
+        private void CompareData(List<Passport> passports)
+        {
+            List<Task> tasks = new List<Task>();
+
+            foreach (Passport passport in passports)
+            {
+                if (_passportsActive.Contains(passport))
+                {
+                    _passportsActive.Remove(passport);
+                    _passportsInactiveActual.Add(passport);
+
+                    _passportsForDeactivation.Enqueue(passport);
+
+                    if (_passportsForDeactivation.Count == _batchSize)
+                    {
+                        //Обновить(деактивировать)
+                        Passport[] passportsBatch = new Passport[_batchSize];
+
+                        for (int i = 0; i < _batchSize; i++)
+                        {
+                            passportsBatch[i] = _passportsForDeactivation.Dequeue();
+                        }
+
+                        tasks.Add(Update(passportsBatch, ActionType.Deactivated));
+                    }
+
+                    continue;
+                }
+
+                if (!_passportsInactive.Contains(passport))
+                {
+                    _passportsInactiveActual.Add(passport);
+
+                    _passportsForInsert.Enqueue(passport);
+
+                    if (_passportsForInsert.Count == _batchSize)
+                    {
+                        //Вставить
+                        Passport[] passportsBatch = new Passport[_batchSize];
+
+                        for (int i = 0; i < _batchSize; i++)
+                        {
+                            passportsBatch[i] = _passportsForInsert.Dequeue();
+                        }
+
+                        tasks.Add(Insert(passportsBatch));
+                    }
+
+                    continue;
+                }
+
+                _passportsInactive.Remove(passport);
+                _passportsInactiveActual.Add(passport);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        private void CompareRemainData()
+        {
+            List<Task> tasks = new List<Task>();
+
+            foreach (Passport passport in _passportsInactive)
+            {
+                _passportsInactive.Remove(passport);
+                _passportsActive.Add(passport);
+
+                _passportsForActivation.Enqueue(passport);
+
+                if (_passportsForActivation.Count == _batchSize)
+                {
+                    //Обновить(активировать)
+                    Passport[] passportsBatch = new Passport[_batchSize];
+
+                    for (int i = 0; i < _batchSize; i++)
+                    {
+                        passportsBatch[i] = _passportsForActivation.Dequeue();
+                    }
+
+                    tasks.Add(Update(passportsBatch, ActionType.Activated));
+                }
+            }
+
+            if (_passportsForActivation.Count > 0)
+            {
+                //Обновить(активировать)
+                int count = _passportsForInsert.Count;
+
+                Passport[] passportsBatch = new Passport[count];
+
+                for (int i = 0; i < count; i++)
+                {
+                    passportsBatch[i] = _passportsForActivation.Dequeue();
+                }
+
+                tasks.Add(Update(passportsBatch, ActionType.Activated));
+            }
+
+            if (_passportsForDeactivation.Count > 0)
+            {
+                //Обновить(деактивировать)
+                int count = _passportsForInsert.Count;
+
+                Passport[] passportsBatch = new Passport[count];
+
+                for (int i = 0; i < count; i++)
+                {
+                    passportsBatch[i] = _passportsForDeactivation.Dequeue();
+                }
+
+                tasks.Add(Update(passportsBatch, ActionType.Deactivated));
+            }
+
+            if (_passportsForInsert.Count > 0)
+            {
+                //Вставить
+                int count = _passportsForInsert.Count;
+
+                Passport[] passportsBatch = new Passport[count];
+
+                for (int i = 0; i < count; i++)
+                {
+                    passportsBatch[i] = _passportsForInsert.Dequeue();
+                }
+
+                tasks.Add(Insert(passportsBatch));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        private void CopyData()
+        {
+            Task inactivePassportsTask = Task.Run(() =>
+            {
+                string inactivePassportsPath = _options.InactivePassportsPath;
+
+                using (StreamWriter writer = new StreamWriter(inactivePassportsPath))
+                {
+                    using (CsvWriter csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                    {
+                        csv.WriteRecords(_passportsInactiveActual);
+                    }
+                }
+            });
+
+            Task activePassportsTask = Task.Run(() =>
+            {
+                string activePassportsPath = _options.ActivePassportsPath;
+
+                using (StreamWriter writer = new StreamWriter(activePassportsPath))
+                {
+                    using (CsvWriter csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                    {
+                        csv.WriteRecords(_passportsActive);
+                    }
+                }
+            });
+
+            Task.WaitAll(inactivePassportsTask, activePassportsTask);
+        }
+
+        private async Task Insert(Passport[] passports)
+        {
+            await _semaphore.WaitAsync();
+
+            try
+            {
+                await _repository.Insert(passports, _batchSize);
+
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private async Task Update(Passport[] passports, ActionType actionType)
+        {
+            await _semaphore.WaitAsync();
+
+            try
+            {
+                await _repository.Update(passports, actionType);
+
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+    }
+}
