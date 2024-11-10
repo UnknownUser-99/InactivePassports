29f2d9ccdfca83265a44d6f07dcfee91e70b071a
Unknown User <artyomshul@gmail.com>
2024-11-10 17:59:13 +0300

    Add contracts

---
+++ Core/InactivePassports.Contracts/Repositories/IOperation.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SqlClient;
+using InactivePassports.Data.Entities.File;
+
+namespace InactivePassports.Contracts.Repositories
+{
+    public interface IOperation
+    {
+        Task InsertOperations(int id, int batchSize, ActionType actionType, SqlConnection connection, SqlTransaction transaction);
+        Task InsertOperations(int[] ids, ActionType actionType, SqlConnection connection, SqlTransaction transaction);
+    }
+}
