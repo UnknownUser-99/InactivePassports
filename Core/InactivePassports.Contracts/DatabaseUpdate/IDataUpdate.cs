477dc03baf9d16b84cbed6a7b85738ff4d8477d2
Unknown User <artyomshul@gmail.com>
2024-11-10 17:57:04 +0300

    Add contracts

---
+++ Core/InactivePassports.Contracts/DatabaseUpdate/IDataUpdate.cs
@@ -0,0 +1,10 @@
+﻿using InactivePassports.Data.Entities.File;
+
+namespace InactivePassports.Contracts.DatabaseUpdate
+{
+    public interface IDataUpdate
+    {
+        void GetData(HashSet<Passport> passportsInactive, HashSet<Passport> passportsActive);
+        void UpdateData(IEnumerable<List<Passport>> passports);
+    }
+}
