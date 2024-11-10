93898c767891b3027c230d757d42ffc346abd472
Unknown User <artyomshul@gmail.com>
2024-11-10 23:50:22 +0300

    Add web api

---
+++ Core/InactivePassports.Data/Entities/WebAPI/DateAction.cs
@@ -0,0 +1,8 @@
+﻿namespace InactivePassports.Data.Entities.WebAPI
+{
+    public record DateAction
+    {
+        public DateOnly Date { get; init; }
+        public string Action { get; init; }
+    }
+}
---
+++ Hosts/InactivePassports.WebAPI/Program.cs
@@ -0,0 +1,37 @@
+using InactivePassports.Contracts.Repositories;
+using InactivePassports.Services.Repositories;
+
+namespace InactivePassports.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services.AddScoped<IWebAPI, WebAPIRepository>(provider => new WebAPIRepository(connectionString));
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
