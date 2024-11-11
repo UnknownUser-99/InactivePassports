1563e76cf50298758e9bd6a8316ec97705788006
Unknown User <artyomshul@gmail.com>
2024-11-11 16:53:15 +0300

    Add tests DatabaseUpdate

---
+++ Tests/InactivePassports.Services.Tests.Unit/Repositories/DatabaseUpdateRepositoryTests.cs
@@ -0,0 +1,90 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using InactivePassports.Contracts.Repositories;
+using InactivePassports.Data.Configurations;
+using InactivePassports.Data.Entities.File;
+using InactivePassports.Services.Repositories;
+using Dapper;
+using Moq;
+using Moq.Dapper;
+
+namespace InactivePassports.Services.Tests.Unit.Repositories
+{
+    [TestClass]
+    public class DatabaseUpdateRepositoryTests
+    {
+        private string _connectionString;
+        private IDatabaseUpdate _repository;
+
+        private Mock<IPassport> _passportRepository;
+        private Mock<IOperation> _operationRepository;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _passportRepository = new Mock<IPassport>();
+            _operationRepository = new Mock<IOperation>();
+            _connectionString = "Server=localhost,1433;Database=master;User=SA;Password=G!17dJp!x9LrT2Kq;";
+            _repository = new DatabaseUpdateRepository(_passportRepository.Object, _operationRepository.Object, _connectionString);
+        }
+
+        [TestMethod]
+        public async Task Insert_Valid_Completed()
+        {
+            var passports = new[]
+            {
+                new Passport { Series = 1234, Number = 123456 },
+                new Passport { Series = 4321, Number = 654321 }
+            };
+
+            int batchSize = 50000;
+            int id = 1;
+
+            _passportRepository
+                .Setup(m => m.GetPassportId(batchSize, It.IsAny<SqlConnection>(), It.IsAny<SqlTransaction>()))
+                .ReturnsAsync(id);
+
+            _passportRepository
+                .Setup(m => m.InsertPassports(passports, id, It.IsAny<SqlConnection>(), It.IsAny<SqlTransaction>()))
+                .Returns(Task.CompletedTask);
+
+            _operationRepository
+                .Setup(m => m.InsertOperations(id, passports.Length, ActionType.Deactivated, It.IsAny<SqlConnection>(), It.IsAny<SqlTransaction>()))
+                .Returns(Task.CompletedTask);
+
+            await _repository.Insert(passports, batchSize);
+
+            _passportRepository.Verify(m => m.GetPassportId(batchSize, It.IsAny<SqlConnection>(), It.IsAny<SqlTransaction>()), Times.Once);
+            _passportRepository.Verify(m => m.InsertPassports(passports, id, It.IsAny<SqlConnection>(), It.IsAny<SqlTransaction>()), Times.Once);
+            _operationRepository.Verify(m => m.InsertOperations(id, passports.Length, ActionType.Deactivated, It.IsAny<SqlConnection>(), It.IsAny<SqlTransaction>()), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task Update_Valid_Completed()
+        {
+            var passports = new[]
+            {
+                new Passport { Series = 1234, Number = 123456 },
+                new Passport { Series = 4321, Number = 654321 }
+            };
+
+            ActionType actionType = ActionType.Deactivated;
+
+            var ids = new[] { 1, 2 };
+
+            _passportRepository
+                .Setup(m => m.UpdatePassports(passports, actionType, It.IsAny<SqlConnection>(), It.IsAny<SqlTransaction>()))
+                .ReturnsAsync(ids);
+
+            _operationRepository
+                .Setup(m => m.InsertOperations(ids, actionType, It.IsAny<SqlConnection>(), It.IsAny<SqlTransaction>()))
+                .Returns(Task.CompletedTask);
+
+            await _repository.Update(passports, actionType);
+
+            _passportRepository.Verify(m => m.UpdatePassports(passports, actionType, It.IsAny<SqlConnection>(), It.IsAny<SqlTransaction>()), Times.Once);
+            _operationRepository.Verify(m => m.InsertOperations(ids, actionType, It.IsAny<SqlConnection>(), It.IsAny<SqlTransaction>()), Times.Once);
+        }
+    }
+}
---
+++ Tests/InactivePassports.Services.Tests.Unit/Repositories/PassportRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+using System.Data.Common;
+using InactivePassports.Contracts.Repositories;
+using InactivePassports.Data.Configurations;
+using InactivePassports.Services.Repositories;
+using Dapper;
+using Moq;
+using Moq.Dapper;
+
+namespace InactivePassports.Services.Tests.Unit.Repositories
+{
+    [TestClass]
+    public class PassportRepositoryTests
+    {
+        private RepositoryOptions _options;
+        private IPassport _repository;
+
+        private Mock<DbConnection> _connection;
+        private Mock<DbTransaction> _transaction;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _connection = new Mock<DbConnection>();
+            _transaction = new Mock<DbTransaction>();
+            _options = new RepositoryOptions { Timeout = 600 };
+            _repository = new PassportRepository(_options);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 1)]
+        [DataRow(2, 50001)]
+        [DataRow(1000, 49950001)]
+        public async Task GetPassportId_Valid_Id(int count, int expectedId)
+        {
+            int batchSize = 50000;
+
+            _connection.SetupDapperAsync(c => c.QuerySingleAsync<int>(It.IsAny<string>(), null, _transaction.Object, null, null)).ReturnsAsync(count);
+
+            var result = await _repository.GetPassportId(batchSize, _connection.Object, _transaction.Object);
+
+            Assert.AreEqual(expectedId, result);
+        }
+    }
+}
